a1f86cd3c9062114575b84b865b86b28d8d1dab6
s104062207 <46620078+s104062207@users.noreply.github.com>
2019-01-12 16:59:52 +0800

    Update execute.sh

--- execute.sh
+++ execute.sh
@@ -16,16 +16,3 @@ hdfs dfs -rm $OUTPUT_FILE
 hdfs dfs -rm -r $OUTPUT_DIR $TEMP_I 
 hadoop jar $JAR pagerank.PageRank $INPUT_SIZE $ITER
 hdfs dfs -getmerge $OUTPUT_DIR $OUTPUT_FILE
-
-INPUT_SIZE=10G
-OUTPUT_FILE=homework/HW5/pagerank_$INPUT_SIZE.out
-
-hdfs dfs -rm $OUTPUT_FILE 
-hdfs dfs -rm -r $OUTPUT_DIR $TEMP_I 
-hadoop jar $JAR pagerank.PageRank $INPUT_SIZE $ITER
-hdfs dfs -getmerge $OUTPUT_DIR $OUTPUT_FILE
-
-hw5-judge 100M 21 "homework/HW5/pagerank_100M.out"
-hw5-judge 1G 22 "homework/HW5/pagerank_1G.out"
-hw5-judge 10G 22 "homework/HW5/pagerank_10G.out"
-hw5-judge 50G 16 "homework/HW5/pagerank_50G.out"
